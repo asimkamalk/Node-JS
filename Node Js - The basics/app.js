41ff015b58fc83151773e37cc5f30b40227a4236
Asim Kamal <asimkamalk@gmail.com>
2025-11-28 02:04:13 +0500

    refactor: Remove request end handling for /message POST

--- Node Js - The basics/app.js
+++ Node Js - The basics/app.js
@@ -21,16 +21,15 @@ const server = http.createServer((req, res) => {
 
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parseBody = Buffer.concat(body).toString();
       console.log(parseBody);
       const message = parseBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
     });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
 
   res.setHeader("Content-Type", "text/HTML");

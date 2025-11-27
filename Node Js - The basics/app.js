391d65c112028d08c55bce5d98e79c5c6cc39623
Asim Kamal <asimkamalk@gmail.com>
2025-11-28 01:38:38 +0500

    Redirecting Requests

--- Node Js - The basics/app.js
+++ Node Js - The basics/app.js
@@ -1,16 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
-      "<body><form><input action = '/message' method = 'POST' type = 'text' name = 'message' ><button type = 'submit'>Send</button></form></body>"
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
     );
-    res.write("<html/>");
+    res.write("</html>");
     return res.end();
   }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/HTML");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");

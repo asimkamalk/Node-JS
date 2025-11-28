108b03230315c0885aac94fd850f8cac04455705
Asim Kamal <asimkamalk@gmail.com>
2025-11-29 04:40:10 +0500

    refactor: Move routes to separate files

--- Working with Express/app.js
+++ Working with Express/app.js
@@ -4,30 +4,14 @@ const bodyParser = require("body-parser");
 const PORT = 3000;
 const app = express();
 
+// Importing routes
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//   console.log("I am in a middleware");
-//   next(); //Allow the request to continue to the next middleware in line
-// });
-
-app.use("/add-product", (req, res, next) => {
-  //   console.log("Another middleware");
-  res.send(
-    '<form action="/product" method= "POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  //   console.log("Another middleware");
-  res.send("<h1>Hello from express</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);

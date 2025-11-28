c78d778c3f6e6f18bb47cedbc34c2a2352e54f55
Asim Kamal <asimkamalk@gmail.com>
2025-11-29 01:41:17 +0500

    Add routes for home and add-product

--- Working with Express/app.js
+++ Working with Express/app.js
@@ -2,13 +2,19 @@ const express = require("express");
 const PORT = 3000;
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("I am in a middleware");
-  next(); //Allow the request to continue to the next middleware in line
+// app.use((req, res, next) => {
+//   console.log("I am in a middleware");
+//   next(); //Allow the request to continue to the next middleware in line
+// });
+
+app.use("/add-product", (req, res, next) => {
+  console.log("Another middleware");
+  res.send("<h1>Hello from express</h1>");
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("Another middleware");
+  res.send("<h1>Hello from express</h1>");
 });
 
 app.listen(PORT, () => {

b809163e88fbb8aa0ce131a3a3753fa2e20390d6
Asim Kamal <asimkamalk@gmail.com>
2025-11-28 23:08:13 +0500

    Add comment to clarify middleware execution flow

--- Working with Express/app.js
+++ Working with Express/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use((req, res, next) => {
   console.log("I am in a middleware");
-  next();
+  next(); //Allow the request to continue to the next middleware in line
 });
 
 app.use((req, res, next) => {
